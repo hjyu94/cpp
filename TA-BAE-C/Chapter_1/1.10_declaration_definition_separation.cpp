e517b549bbf3304e511e0f1118e507a9c17db1dc
Hyojeong Yu <hjeong.you@gmail.com>
2023-04-25 09:13:18 +0900

    [1.10] add identifier not found
    main 함수가 눈에 잘 안들어와서 위로 올리면 컴파일 에러 발생

--- TA-BAE-C/Chapter_1/1.10_declaration_definition_separation.cpp
+++ TA-BAE-C/Chapter_1/1.10_declaration_definition_separation.cpp
@@ -2,6 +2,13 @@
 
 using namespace std;
 
+int main()
+{
+	cout << add(1, 2) << endl;
+
+	return 0;
+}
+
 int add(int a, int b)
 {
 	return a + b;
@@ -15,11 +22,4 @@ int multiply(int a, int b)
 int subtract(int a, int b)
 {
 	return a - b;
-}
-
-int main()
-{
-	cout << add(1, 2) << endl;
-
-	return 0;
 }

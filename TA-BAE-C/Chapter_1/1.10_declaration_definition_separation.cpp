08e946d0094f0c0079923dfc094d48967a7d5261
Hyojeong Yu <hjeong.you@gmail.com>
2023-04-25 09:12:39 +0900

    [1.10] 선언과 정의의 분리

---
+++ TA-BAE-C/Chapter_1/1.10_declaration_definition_separation.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int add(int a, int b)
+{
+	return a + b;
+}
+
+int multiply(int a, int b)
+{
+	return a * b;
+}
+
+int subtract(int a, int b)
+{
+	return a - b;
+}
+
+int main()
+{
+	cout << add(1, 2) << endl;
+
+	return 0;
+}

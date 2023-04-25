b2634b4b9201b84716fca46c2c712c7ced30f6d5
Hyojeong Yu <hjeong.you@gmail.com>
2023-04-25 09:30:51 +0900

    [6.17] C++11 for-each

---
+++ TA-BAE-C/Chapter_6/6.17_for_each.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int fibonacci[] = { 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89 };
+
+	for (int number : fibonacci)
+		cout << number << " ";
+	cout << endl;
+
+}

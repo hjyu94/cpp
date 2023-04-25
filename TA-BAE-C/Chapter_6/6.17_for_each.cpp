57e828e9eef92626fd1e069727a392c824d2f150
Hyojeong Yu <hjeong.you@gmail.com>
2023-04-25 09:33:57 +0900

    [6.17] reference & 사용 후, const in for each

--- TA-BAE-C/Chapter_6/6.17_for_each.cpp
+++ TA-BAE-C/Chapter_6/6.17_for_each.cpp
@@ -6,10 +6,11 @@ int main()
 {
 	int fibonacci[] = { 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89 };
 
-	for (int number : fibonacci)
-		number = 10;
+	for (auto &number : fibonacci)
+		number *= 10;
 
-	for (int number : fibonacci)
+	// 이 반복문 안에서는 number 를 변경하지 않겠다.
+	for (const auto &number : fibonacci)
 		cout << number << " ";
 	cout << endl;
 
